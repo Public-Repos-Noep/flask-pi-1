ab40797b2b3bb5ca53e9861df5cb94ba6c441b00
Taehoon <noep@naver.com>
2016-09-25 18:32:24 +0900

    카메라 모듈 추가

--- views/camera.py
+++ views/camera.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Response
+import camera
 
 blueprint = Blueprint('camera', __name__, template_folder='templates')
 
@@ -6,3 +7,18 @@ blueprint = Blueprint('camera', __name__, template_folder='templates')
 @blueprint.route('/', methods=['GET'])
 def camera():
     return render_template('camera.html')
+
+
+@blueprint.route('/feed', methods=['GET'])
+def feed():
+    return Response(gen(camera.Camera()),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+
