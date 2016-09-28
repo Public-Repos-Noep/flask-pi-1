fadd5da127255f88904c13e920578564e9248acb
taehoon <noep@naver.com>
2016-09-28 10:20:52 +0900

    주석 추가

--- views/cam.py
+++ views/cam.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import io
 from flask import Blueprint, render_template, Response
 # from camera.camera_pi import Camera
@@ -6,25 +7,29 @@ from camera.camera_dummy import Camera
 blueprint = Blueprint('camera', __name__, template_folder='templates')
 
 
+#  카메라 메인 웹 뷰
 @blueprint.route('/', methods=['GET'])
 def camera():
     return render_template('camera.html')
 
 
+#  사진찍기 웹 뷰
 @blueprint.route('/shoot', methods=['GET'])
 def shoot():
     print('/camera/shoot')
     return render_template('shoot.html')
 
 
+#  카메라 메인에서 요청하는 미리보기 스트리밍
 @blueprint.route('/feed', methods=['GET'])
 def feed():
     print('/camera/feed')
     camera = Camera()
     return Response(gen(camera, True)
-                    ,mimetype='multipart/x-mixed-replace; boundary=frame')
+                    , mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+#  사진찍기 웹 뷰에서 요청하는 사진찍기
 @blueprint.route('/picture', methods=['GET'])
 def picture():
     camera = Camera()
@@ -33,23 +38,21 @@ def picture():
                     , mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-
 def gen(camera, isStream):
-
-    if isStream is True :
+    if isStream is True:
         while True:
             frame = camera.get_frame()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
     else:
         frame = camera.get_frame()
-        fileSave(frame)
+        filesave(frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-def fileSave(frame):
-    with open('temp.jpeg','wb') as file:
+def filesave(frame):
+    with open('temp.jpeg', 'wb'):
         file.write(frame)
     print 'file saved'
     return True
