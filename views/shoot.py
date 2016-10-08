56e228236ce6f70a424579c29af6b7c420354406
Taehoon <noep@naver.com>
2016-10-08 19:11:17 +0900

    filesave 파일 저장 버그 해결

--- views/shoot.py
+++ views/shoot.py
@@ -17,27 +17,21 @@ def shoot():
 #  사진찍기 웹 뷰에서 요청하는 사진찍기
 @blueprint.route('/feed', methods=['GET'])
 def feed():
-    camera = Camera()
-
-    return Response(getframe(camera)
+    frame = getframe(Camera())
+    return Response(frame
                     , mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-
-
-
-#단일 출력
+# 단일 출력
 def getframe(camera):
     frame = camera.get_frame()
-    #todo filesave는 멀티파트로 입력받을때
-    #filesave(frame)
+    filesave(frame)
     return (b'--frame\r\n'
-           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
+# coding=ascii
 def filesave(frame):
-    with open('temp.jpeg', 'wb'):
-        file.write(frame)
+    with open('temp.jpeg', 'wb') as f:
+        f.write(frame)
     print 'file saved'
     return True
-
